a58e2c443cf521313f522fbbb27ed076091c5725
cengamat <azurnaci@cultwinesltd.com>
2023-01-28 23:47:40 +0300

    Making our 1st API call

--- cenggpt/Program.cs
+++ cenggpt/Program.cs
@@ -1,8 +1,19 @@
-﻿if (args.Length > 0)
+﻿using System.Text;
+
+if (args.Length > 0)
 {
     HttpClient client = new HttpClient();
 
-    client.DefaultRequestHeaders.Add("authorization", "Bearer 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")
+    client.DefaultRequestHeaders.Add("authorization", "Bearer <INSERT YOUR API KEY HERE>");
+
+    var content = new StringContent("{\"model\": \"text-davinci-001\", \"prompt\": \"" + args[0] + "\",\"temperature\": 1,\"max_tokens\": 100}",
+        Encoding.UTF8, "application/json");
+
+    HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+
+    string responseString = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(responseString);
 }
 else
 {
