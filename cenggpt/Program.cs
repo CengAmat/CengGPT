aa1397d9af6e55aa27aff6c6bd3ed982d47ebe4a
cengamat <azurnaci@cultwinesltd.com>
2023-01-28 23:20:16 +0300

    packages added, api key generated

---
+++ cenggpt/Program.cs
@@ -0,0 +1,10 @@
+﻿if (args.Length > 0)
+{
+    HttpClient client = new HttpClient();
+
+    client.DefaultRequestHeaders.Add("authorization", "Bearer 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")
+}
+else
+{
+    Console.WriteLine("---> You need to provide some input");
+}
