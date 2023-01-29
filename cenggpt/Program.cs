a4b7c2fe4bc4635d6ccd15b139ca59a13b1b7454
cengamat <azurnaci@cultwinesltd.com>
2023-01-29 14:31:40 +0300

    Extracting the command prompt

--- cenggpt/Program.cs
+++ cenggpt/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using Newtonsoft.Json;
 
 if (args.Length > 0)
 {
@@ -13,9 +14,40 @@ if (args.Length > 0)
 
     string responseString = await response.Content.ReadAsStringAsync();
 
-    Console.WriteLine(responseString);
+    try
+    {
+        var dyData = JsonConvert.DeserializeObject<dynamic>(responseString);
+
+        string guess = GuessCommand(dyData.choices[0].text);
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"--> My guess at the command prompt is: {guess}");
+        Console.ResetColor();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"---> Could not deserialize the JSON: {ex.Message}");
+    }
+    // Console.WriteLine(responseString);
 }
 else
 {
     Console.WriteLine("---> You need to provide some input");
+}
+
+static string GuessCommand(string raw)
+{
+    Console.WriteLine("--> GPT-3 API Returned Text:");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine(raw);
+
+    var lastIndex = raw.LastIndexOf('\n');
+
+    string guess = raw.Substring(lastIndex + 1);
+
+    Console.ResetColor();
+
+    TextCopy.ClipboardService.SetText(guess);
+
+    return guess;
 }
